e4fa114d17db303d3b63eb384e4bce9e7dd6d9e5
fingibbo <fingibbo@hotmail.co.uk>
2021-04-26 18:06:53 +0100

    Add project files.

---
+++ csharpClient/LogInPage.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace csharpClient
+{
+    partial class LogInPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logInButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.receiveListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // logInButton
+            // 
+            this.logInButton.Location = new System.Drawing.Point(12, 137);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(75, 25);
+            this.logInButton.TabIndex = 2;
+            this.logInButton.Text = "Log In";
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(216, 137);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 25);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(17, 85);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(273, 20);
+            this.usernameTextBox.TabIndex = 5;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(17, 111);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(273, 20);
+            this.passwordTextBox.TabIndex = 6;
+            // 
+            // receiveListBox
+            // 
+            this.receiveListBox.FormattingEnabled = true;
+            this.receiveListBox.Location = new System.Drawing.Point(17, 174);
+            this.receiveListBox.Name = "receiveListBox";
+            this.receiveListBox.Size = new System.Drawing.Size(120, 95);
+            this.receiveListBox.TabIndex = 7;
+            // 
+            // LogInPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 281);
+            this.Controls.Add(this.receiveListBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.logInButton);
+            this.Name = "LogInPage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.ListBox receiveListBox;
+    }
+}
+
---
+++ csharpClient/LogInPage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace csharpClient
+{
+    public partial class LogInPage : Form
+    {
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("86.6.1.8"), 420); //
+
+        public LogInPage()
+        {
+            InitializeComponent();
+
+
+        }
+        public void msgSender(string toSend)
+        {
+            int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+            byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+            byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+            clientSocket.Send(toSendLenBytes);
+            clientSocket.Send(toSendBytes);
+        }
+        
+        public void msgReceiver()
+        {
+            byte[] rcvLenBytes = new byte[4];
+            clientSocket.Receive(rcvLenBytes);
+            int rcvLen = System.BitConverter.ToInt32(rcvLenBytes, 0);
+            byte[] rcvBytes = new byte[rcvLen];
+            clientSocket.Receive(rcvBytes);
+            String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
+            BeginInvoke((Action)(() =>
+            {
+                receiveListBox.Items.Add(rcv);
+                receiveListBox.SelectedIndex = receiveListBox.Items.Count - 1;
+            }));
+        }
+        
+
+
+        private void logInButton_Click(object sender, EventArgs e)
+        {
+            clientSocket.Connect(serverAddress);
+
+            msgSender("U" + usernameTextBox.Text);
+            msgSender("P" + passwordTextBox.Text);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            if (!(sender is Button))
+            {
+                return;
+            }
+            Application.Exit();
+        }
+    }
+}
